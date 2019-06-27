1d4c69b2aabc0a40f71735aa2949949f59c205f6
Andrew Sepúlveda <andrews9@fb.com>
2019-06-27 12:08:39 -0700

    whole proj

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by andrews9 on 6/27/19.
+//  Copyright © 2019 andrews9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *displayPhoto;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/PhotosViewController.h
@@ -0,0 +1,18 @@
+//
+//  PhotosViewController.h
+//  Tumblr
+//
+//  Created by andrews9 on 6/27/19.
+//  Copyright © 2019 andrews9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIImageView+AFNetworking.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
