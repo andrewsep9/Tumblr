473b4bf184bc39907662fd14c50af55e79e1c694
Andrew Sepúlveda <andrews9@fb.com>
2019-06-27 10:29:37 -0700

    Initial Commit

---
+++ Tumblr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tumblr
+//
+//  Created by andrews9 on 6/27/19.
+//  Copyright © 2019 andrews9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
